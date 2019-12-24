0fd5ac051c2e53946180549045313da2753d577c
Hou Jue <jxhou@lx9-400-12082.hum.helsinki.fi>
2019-12-24 15:23:49 +0200

    deploy

--- index.js
+++ index.js
@@ -33,9 +33,6 @@ let persons = [
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
 
 app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info of ${persons.length} people</p><p>${new Date()}</p`)
